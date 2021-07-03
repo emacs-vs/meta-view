c0d1846e8dbe511a0967e906087a9943512d0db2
JenChieh <jcs090218@gmail.com>
2021-07-04 00:12:16 +0800

    Update core

--- meta-view.el
+++ meta-view.el
@@ -7,7 +7,7 @@
 ;; Description: View metadata from .NET assemblies
 ;; Keyword: assembly metadata source
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.3") (csharp-mode "0.11.0") (meta-net "1.0.0"))
+;; Package-Requires: ((emacs "24.3") (csharp-mode "0.11.0") (meta-net "1.0.0") (ht "2.3"))
 ;; URL: https://github.com/emacs-vs/meta-view
 
 ;; This file is NOT part of GNU Emacs.
@@ -32,7 +32,10 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+
 (require 'csharp-mode)
+(require 'ht)
 (require 'meta-net)
 
 (defgroup meta-view nil
@@ -47,23 +50,63 @@
   :type 'list
   :group 'meta-view)
 
+(defconst meta-view--templates-dir
+  (let ((dir (file-name-directory (find-library-name "meta-view"))))
+    (concat dir "templates/"))
+  "Templates path for package `meta-view'.")
+
 (defconst meta-view--buffer-name "* %s [from metadata] *"
   "Buffer name to display metadata.")
 
 (defvar meta-view--buffer nil
   "Singleton, it records the displayed buffer.")
 
+(defvar-local meta-view--xmls nil
+  "Cache records a list of assembly xml file path.")
+
+;;
+;; (@* "Util" )
+;;
+
 (defmacro meta-view--with-buffer (name &rest body)
   "Execute BODY inside the metadata displayed buffer with NAME."
   (declare (indent 0) (debug t))
   `(let ((buf-name (format meta-view--buffer-name name)))
-     (with-current-buffer (get-buffer-create buf-name) (progn ,@body))))
+     (with-current-buffer (get-buffer-create buf-name)
+       (setq meta-view--buffer (current-buffer))
+       (csharp-mode) (progn ,@body))))
 
 (defun meta-view--kill-display-buffer ()
   "Kill the metadata display buffer."
   (when (buffer-live-p meta-view--buffer)
     (kill-buffer meta-view--buffer)
     (setq meta-view--buffer nil)))
+
+;;
+;; (@* "Xmls" )
+;;
+
+(defun meta-view--get-xmls ()
+  "Return a list of assembly xml files.
+
+We use these path as search index for variable `meta-net-xml'."
+  (let (xmls)
+    (dolist (path (meta-net-csporj-files))
+      (setq xmls (append (meta-net-csproj-xmls path))))
+    xmls))
+
+(defun meta-view--all-xmls (&optional refresh)
+  "Return full list of assembly xml files.
+
+If REFRESH is non-nil, refresh cache once."
+  (when (or refresh (null meta-view--xmls))
+    (setq meta-view--xmls (meta-view--get-xmls))
+    (cl-delete-duplicates meta-view--xmls))
+  meta-view--xmls)
+
+;;
+;; (@* "Core" )
+;;
 
 ;;;###autoload
 (defun meta-view-at-point ()
@@ -72,13 +115,13 @@
   (meta-view (thing-at-point 'symbol)))
 
 ;;;###autoload
-(defun meta-view (&optional name)
+(defun meta-view (name)
   "View metadata by NAME."
   (interactive)
   (unless (memq major-mode meta-view-active-modes)
     (user-error "Invalid major-mode to view metadata, %s" major-mode))
   (unless (stringp name) (user-error "Invalid name to view metadata, %s" name))
-  (meta-net-read-project)  ; read it
+  (unless meta-net-csproj-current (meta-net-read-project))
   (meta-view--kill-display-buffer)
   )
 
